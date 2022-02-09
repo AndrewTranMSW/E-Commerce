d933c7c5a08f0b118c605c7b88a7c3d0e5ab907f
Andrew Tran <andrewtranmsw@gmail.com>
2022-02-09 08:31:36 -0800

    boilierplate server.js completed

---
+++ server.js
@@ -0,0 +1,19 @@
+//Default express and routes
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//Default middleware for parsing
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//route initializing
+app.use(routes);
+
+//syncs Sequelize models to the db and turns on the server
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}.`);
+});
+
